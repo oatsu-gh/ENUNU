85249c86cc23ed97230f1cea3eb47ff4c0c61e21
oatsu <panchi.psp@gmail.com>
2021-07-20 00:14:38 +0900

    Update run.sh
    enunu_training_kit からコピー

--- train/run.sh
+++ train/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##########
-# customized for nnsvs on WSL(Ubuntu)
+# customized for ENUNU-Training-Kit on Windows
 ##########
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
@@ -15,19 +15,21 @@ function xrun () {
     set +x
 }
 
-#change-----------------------------------------------------------
-NNSVS_ROOT=../nnsvs
-#change-----------------------------------------------------------
+# use embed python executional file
+alias python="./../python-3.8.10-embed-amd64/python"
+
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+NNSVS_ROOT=../nnsvs
 NNSVS_COMMON_ROOT=$NNSVS_ROOT/egs/_common/spsvs
 NO2_ROOT=$NNSVS_ROOT/egs/_common/no2
-. $NNSVS_ROOT/utils/yaml_parser.sh || exit 1;
+. "$NNSVS_ROOT/utils/yaml_parser.sh" || exit 1;
 
 eval $(parse_yaml "./config.yaml" "")
 
-train_set="train_no_dev"
-dev_set="dev"
-eval_set="eval"
+train_set=train_no_dev
+dev_set=dev
+eval_set=eval
 datasets=($train_set $dev_set $eval_set)
 testsets=($dev_set $eval_set)
 
@@ -48,6 +50,8 @@ else
 fi
 expdir=exp/$expname
 
+
+# assert singing-database exits
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
     if [ ! -e $db_root ]; then
 	cat<<EOF
@@ -56,40 +60,75 @@ EOF
     fi
 fi
 
+
+# Prepare files in singing-database for training
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    echo "stage 0: Data preparation"
-    #change↓------------------------------------------------------
+    echo "##########################################"
+    echo "#  stage 0: Data preparation             #"
+    echo "##########################################"
     rm -rf data dump
     bash data_prep.sh ./config.yaml
-    #change↑------------------------------------------------------
 fi
 
+
+# Analyze .wav and .lab files
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    echo "stage 1: Feature generation"
+    echo "##########################################"
+    echo "#  stage 1: Feature generation           #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/feature_generation.sh
 fi
 
+
+# Train time-lag model
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    echo "stage 2: Training time-lag model"
+    echo "##########################################"
+    echo "#  stage 2: Time-lag model training      #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_timelag.sh
 fi
 
+
+# Train duration model
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    echo "stage 3: Training duration model"
+    echo "##########################################"
+    echo "#  stage 3: Duration model training      #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_duration.sh
 fi
 
+
+# Train acoustic model
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    echo "stage 4: Training acoustic model"
+    echo "##########################################"
+    echo "#  stage 4: Training acoustic model      #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_acoustic.sh
 fi
 
+
+# Generate models from timelag/duration/acoustic models
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    echo "stage 5: Generate features from timelag/duration/acoustic models"
+    echo "##########################################"
+    echo "#  stage 5: Feature generation           #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/generate.sh
 fi
 
+
+# Synthesis wav files
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-    echo "stage 6: Synthesis waveforms"
+    echo "##########################################"
+    echo "#  stage 6: Waveform synthesis           #"
+    echo "##########################################"
     . $NNSVS_COMMON_ROOT/synthesis.sh
+fi
+
+
+# Copy the models to release directory
+if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
+    echo "##########################################"
+    echo "#  stage 7: Release preparation          #"
+    echo "##########################################"
+    python prepare_model_for_release_enunu.py
 fi
