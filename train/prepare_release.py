b18ae9c059864fc118738bd81fe5190ce6412198
oatsu <panchi.psp@gmail.com>
2021-07-20 00:14:03 +0900

    ファイル名変更

--- train/prepare_release.py
+++ train/prepare_release.py
@@ -1,25 +1,17 @@
-#! /usr/bin/env python3
-# coding: utf-8
-# Copyright (c) oatsu
+#!/usr/bin/env python3
+# Copyright (c) 2021 oatsu
 """
 配布用フォルダを準備する
 """
 
 from glob import glob
 from os import makedirs
+from os.path import exists
 from shutil import copy2, copytree
 
 import yaml
+from send2trash import send2trash
 from tqdm import tqdm
-
-with open('config.yaml', 'r') as f_yaml:
-    config = yaml.load(f_yaml, Loader=yaml.FullLoader)
-SINGER = config['spk']
-CONFIG_DIR = 'conf'
-DICTIONARY_DIR = config['sinsy_dic']
-RELEASE_DIR = f'release/{SINGER}_---'
-PATH_QUESTION = config['question_path']
-NAME_EXPERIMENT = config['tag']
 
 
 def copy_train_config(config_dir, release_dir):
@@ -30,20 +22,21 @@ def copy_train_config(config_dir, release_dir):
     copytree(config_dir, f'{release_dir}/{config_dir}')
 
 
-def copy_dictionary(dictionary_dir, release_dir):
+def copy_dictionary(path_table, release_dir):
     """
     *.table, *.conf をコピー
     """
     print('copying dictionary')
-    copytree(dictionary_dir, f'{release_dir}/{dictionary_dir}')
+    makedirs(f'{release_dir}/dic', exist_ok=True)
+    copy2(path_table, f'{release_dir}/{path_table}')
 
 
 def copy_question(path_question, release_dir):
     """
     hedファイル(question)をコピー
     """
-    makedirs(f'{release_dir}/hed', exist_ok=True)
     print('copying question')
+    makedirs(f'{release_dir}/hed', exist_ok=True)
     copy2(path_question, f'{release_dir}/{path_question}')
 
 
@@ -74,6 +67,7 @@ def copy_model(singer, name_exp, release_dir):
     for path_model in tqdm(list_path_model):
         copy2(path_model, f'{release_dir}/{path_model}')
 
+
 def copy_general_config(release_dir):
     """
     singer: 歌唱者名
@@ -82,17 +76,40 @@ def copy_general_config(release_dir):
     copy2('config.yaml', f'{release_dir}/config.yaml')
 
 
+def copy_enuconfig(release_dir):
+    """
+    singer: 歌唱者名
+    """
+    print('copying enuconfig.yaml')
+    copy2('enuconfig.yaml', f'{release_dir}/enuconfig.yaml')
+
+
 def main():
     """
     各種ファイルをコピーする
     """
-    makedirs(RELEASE_DIR, exist_ok=True)
-    copy_general_config(RELEASE_DIR)
-    # copy_train_config(CONFIG_DIR, RELEASE_DIR)
-    copy_dictionary(DICTIONARY_DIR, RELEASE_DIR)
-    copy_question(PATH_QUESTION, RELEASE_DIR)
-    copy_scaler(SINGER, RELEASE_DIR)
-    copy_model(SINGER, NAME_EXPERIMENT, RELEASE_DIR)
+    # load settings
+    with open('config.yaml', 'r') as f_yaml:
+        config = yaml.load(f_yaml, Loader=yaml.FullLoader)
+    singer = config['spk'].strip('"\'')
+    config_dir = 'conf'
+    release_dir = f'release/{singer}_---'
+    path_table = config['table_path'].strip('"\'')
+    path_question = config['question_path'].strip('"\'')
+    experiment_name = config['tag'].strip('"\'')
+
+    # copy models to the release directory
+    if exists(release_dir):
+        print('Sending existing directory to recycle bin')
+        send2trash(release_dir)
+    makedirs(release_dir, exist_ok=True)
+    copy_general_config(release_dir)
+    copy_enuconfig(release_dir)
+    copy_train_config(config_dir, release_dir)
+    copy_dictionary(path_table, release_dir)
+    copy_question(path_question, release_dir)
+    copy_scaler(singer, release_dir)
+    copy_model(singer, experiment_name, release_dir)
 
 
 if __name__ == '__main__':
