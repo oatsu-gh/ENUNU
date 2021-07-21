c85d830cd2c3ed318741577521419f113b2626c5
oatsu <panchi.psp@gmail.com>
2021-07-22 01:57:59 +0900

    Update run.sh

--- train/run.sh
+++ train/run.sh
@@ -16,13 +16,13 @@ function xrun () {
 }
 
 # use embed python executional file
-alias python="./../python-3.8.10-embed-amd64/python"
+alias python="python-3.8.10-embed-amd64/python"
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-NNSVS_ROOT=../nnsvs
-NNSVS_COMMON_ROOT=$NNSVS_ROOT/egs/_common/spsvs
-NO2_ROOT=$NNSVS_ROOT/egs/_common/no2
+NNSVS_ROOT="nnsvs"
+NNSVS_COMMON_ROOT="$NNSVS_ROOT/egs/_common/spsvs"
+NO2_ROOT="$NNSVS_ROOT/egs/_common/no2"
 . "$NNSVS_ROOT/utils/yaml_parser.sh" || exit 1;
 
 eval $(parse_yaml "./config.yaml" "")
@@ -34,8 +34,8 @@ datasets=($train_set $dev_set $eval_set)
 testsets=($dev_set $eval_set)
 
 dumpdir=dump
-dump_org_dir=$dumpdir/$spk/org
-dump_norm_dir=$dumpdir/$spk/norm
+dump_org_dir="$dumpdir/$spk/org"
+dump_norm_dir="$dumpdir/$spk/norm"
 
 stage=0
 stop_stage=0
@@ -44,11 +44,11 @@ stop_stage=0
 
 # exp name
 if [ -z ${tag:=} ]; then
-    expname=${spk}
+    expname="${spk}"
 else
-    expname=${spk}_${tag}
+    expname="${spk}_${tag}"
 fi
-expdir=exp/$expname
+expdir="exp/$expname"
 
 
 # assert singing-database exits
@@ -63,9 +63,11 @@ fi
 
 # Prepare files in singing-database for training
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    echo "##########################################"
-    echo "#  stage 0: Data preparation             #"
-    echo "##########################################"
+    echo "#########################################"
+    echo "#                                       #"
+    echo "#  stage 0: Data preparation            #"
+    echo "#                                       #"
+    echo "#########################################"
     rm -rf $out_dir $dumpdir
     python preprocess_data.py ./config.yaml
     echo ""
@@ -75,7 +77,9 @@ fi
 # Analyze .wav and .lab files
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 1: Feature generation           #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/feature_generation.sh
     echo ""
@@ -85,7 +89,9 @@ fi
 # Train time-lag model
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 2: Time-lag model training      #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_timelag.sh
     echo ""
@@ -95,7 +101,9 @@ fi
 # Train duration model
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 3: Duration model training      #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_duration.sh
     echo ""
@@ -105,7 +113,9 @@ fi
 # Train acoustic model
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 4: Training acoustic model      #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/train_acoustic.sh
     echo ""
@@ -115,7 +125,9 @@ fi
 # Generate models from timelag/duration/acoustic models
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 5: Feature generation           #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/generate.sh
     echo ""
@@ -125,7 +137,9 @@ fi
 # Synthesis wav files
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 6: Waveform synthesis           #"
+    echo "#                                        #"
     echo "##########################################"
     . $NNSVS_COMMON_ROOT/synthesis.sh
     echo ""
@@ -135,8 +149,10 @@ fi
 # Copy the models to release directory
 if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
     echo "##########################################"
+    echo "#                                        #"
     echo "#  stage 7: Release preparation          #"
+    echo "#                                        #"
     echo "##########################################"
-    python prepare_model_for_release_enunu.py
+    python prepare_release.py
     echo ""
 fi
