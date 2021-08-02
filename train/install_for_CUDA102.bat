9fe1c621b04f00211b6b5da44fa4f8dfbaa6235e
oatsu <panchi.psp@gmail.com>
2021-08-03 00:16:12 +0900

    環境構築バッチを追加

---
+++ train/install_for_CUDA102.bat
@@ -0,0 +1,14 @@
+@REM ENUNU Portable Train Kit 用の環境構築バッチファイル
+@REM CUDA 10.2 環境向け
+
+pip install --upgrade pip
+pip install wheel
+pip install numpy cython
+pip install hydra-core<1.1
+pip install tqdm pydub pyyaml natsort
+pip install --upgrade utaupy
+pip install --upgrade nnmnkwii
+pip install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+git clone "https://github.com/r9y9/nnsvs"
+pip install ./nnsvs
