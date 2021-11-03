361baa0d49ef6f4a7416de227de2f8007a32219c
oatsu <panchi.psp@gmail.com>
2021-11-03 22:43:05 +0900

    Update train_acoustic.sh
    見た目をいじった

--- train/nnsvs_shell_scripts/train_acoustic.sh
+++ train/nnsvs_shell_scripts/train_acoustic.sh
@@ -39,7 +39,9 @@ else
     resume_checkpoint=
 fi
 xrun $PYTHON_EXE -m nnsvs.bin.train $ext \
-    model=$acoustic_model train=$acoustic_train data=$acoustic_data \
+    model=$acoustic_model \
+    train=$acoustic_train \
+    data=$acoustic_data \
     data.train_no_dev.in_dir=$dump_norm_dir/$train_set/in_acoustic/ \
     data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_acoustic/ \
     data.dev.in_dir=$dump_norm_dir/$dev_set/in_acoustic/ \
