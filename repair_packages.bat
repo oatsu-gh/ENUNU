e9cae13c9ab8643aa6520a8c9e7fff5447ea52c1
oatsu <oatsu.dev@gmail.com>
2025-12-14 00:46:47 +0900

    Update repair_packages.bat

--- repair_packages.bat
+++ repair_packages.bat
@@ -1,12 +1,12 @@
-@echo Pytorch と utaupy を再インストールします。数分かかるので気長にお待ちください。
-@echo Re-installing packages. Please wait some minutes.
+@echo Re-installing Python packages. Please wait some minutes.
 
 nvcc -V
 
-@set python_exe=%~dp0..\python-3.12.10-embed-amd64\python.exe
-%python_exe% -m pip uninstall torch torchaudio torchvision --quiet -y --no-warn-script-location
-%python_exe% -m pip install --upgrade utaupy --no-warn-script-location
-%python_exe% -m pip install --upgrade light_the_torch --quiet --no-warn-script-location
-%python_exe% -m light_the_torch install torch torchaudio --no-warn-script-location
+@set python_exe=%~dp0\python-3.12.10-embed-amd64\python.exe
+%python_exe% -m pip uninstall torch torchaudio torchvision --quiet -y
+
+%python_exe% -m pip install --upgrade utaupy --no-warn-script-location --disable-pip-version-check
+%python_exe% -m pip install --upgrade light_the_torch --quiet --no-warn-script-location --disable-pip-version-check
+%python_exe% -m light_the_torch install torch torchaudio --no-warn-script-location --disable-pip-version-check
 
 PAUSE
