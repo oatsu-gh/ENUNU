2108b1ce8a56f15f82a830aced2ef0b6d6c1a9b1
oatsu <oatsu.dev@gmail.com>
2025-12-14 00:46:34 +0900

    update utils/github_download_count_*.py

--- utils/github_download_count_enunu.py
+++ utils/github_download_count_enunu.py
@@ -1,11 +1,12 @@
 import requests
 
-r = requests.get('https://api.github.com/repos/oatsu-gh/ENUNU/releases')
+r = requests.get('https://api.github.com/repos/oatsu-gh/ENUNU/releases', timeout=10)
+
 for item in r.json():
-    print("tag_name: ",item["tag_name"])
-    print("name: ", item["name"])
-    print(item["assets"][0]["url"])
-    print("download count: ", item["assets"][0]["download_count"])
-    print("")
+    print('tag_name: ', item['tag_name'])
+    print('name: ', item['name'])
+    print(item['assets'][0]['url'])
+    print('download count: ', item['assets'][0]['download_count'])
+    print('')
 
 input()
--- utils/github_download_count_simpleenunu.py
+++ utils/github_download_count_simpleenunu.py
@@ -1,15 +1,12 @@
 from pprint import pprint
-
 import requests
 
-r = requests.get(
-    'https://api.github.com/repos/oatsu-gh/simpleenunu/releases')
-
+r = requests.get('https://api.github.com/repos/oatsu-gh/simpleenunu/releases', timeout=10)
 
 pprint(r.json())
 for item in r.json():
-    print("tag_name: ", item["tag_name"])
-    print("name: ", item["name"])
+    print('tag_name: ', item['tag_name'])
+    print('name: ', item['name'])
     # print(item["assets"][0]["url"])
-    print("download count: ", item["assets"][0]["download_count"])
-    print("")
+    print('download count: ', item['assets'][0]['download_count'])
+    print('')
