6ed8a7d673550d6d69b18b951eb8b621717bc91a
oatsu <oatsu.dev@gmail.com>
2025-12-14 16:11:30 +0900

    Update __init__.py

--- enulib/__init__.py
+++ enulib/__init__.py
@@ -1,1 +1,2 @@
-from . import enunu2nnsvs, extensions, install_torch, utauplugin2score  # noqa: F401
+from . import extensions, install_torch, utauplugin2score  # noqa: F401
+# enunu2nnsvs は torch を要求してしまうので個別import必須にする。
